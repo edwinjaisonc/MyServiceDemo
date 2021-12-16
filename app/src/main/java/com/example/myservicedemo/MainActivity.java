fa7edd1a6f123538bcf26cc55fc417809de2438b
quest <edwinjaisonc@gmail.com>
2021-12-16 10:27:24 +0530

    Commit

--- app/src/main/java/com/example/myservicedemo/MainActivity.java
+++ app/src/main/java/com/example/myservicedemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myservicedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this,CameraService.class);
+        startService(new Intent(this,CameraService.class));
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            startForegroundService(intent);
+        }else {
+            this.startService(intent);
+        }
     }
 }
